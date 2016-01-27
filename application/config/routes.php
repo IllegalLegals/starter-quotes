ef2215c53e27ff5222a1abac1f011dae14648643
Jon Deluz <jondeluz@hotmail.com>
2016-01-27 13:42:53 -0800

    Fixed link #6 and updated changelog

--- application/config/routes.php
+++ application/config/routes.php
@@ -43,6 +43,18 @@ $route['sleep'] = 'first/zzz';
 $route['404_override'] = '';
 
 $route['lock/(:any)/(:any)'] = 'welcome/shucks';
+$route['dunno'] = function(){
+    $source = "data/at.jpg";
 
+    if (!file_exists($source)) {
+        show_404($source);
+    } else {
+    //  $this->load->helper('file');
+    //  $mimeType = get_mime_by_extension($source);
+        header("Content-type: image/jpeg");
+        header('Content-Disposition: inline');
+        readfile($source);
+    }
+};
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
